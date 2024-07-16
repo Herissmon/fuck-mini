c857cb713a660869f926577d50436a9c103b6d53
Herissmon <cnjn789@163.com>
2024-07-16 10:25:26 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,36 +1,36 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'mc网站挑战赛',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur2616492-fb340e2ab082d66b5cce7748',
   ],
 
   // 日志天数
   CountDays: 90,
 
   // 是否显示检测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '目前在进行挑战赛的网站：',
+      url: '#'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      text: 'minebbs',
+      url: 'www.minebbs.com'
     },
     {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      text: '我的世界服务端镜像站',
+      url: 'minecraft-mirror.pages.dev'
     },
+    {
+      text: '已经淘汰了的网站:',
+      url: '#'
   ],
 };
